818f285c52e6cc79d4335eeb08392370582ebe5b
VNGrim <dodinhvan01@gmail.com>
2026-06-24 12:29:50 +0700

    Create phepcanbac2.java

---
+++ src/phepcanbac2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class phepcanbac2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.print(String.format("%.2f", Math.sqrt(a))); //Math.sqrt(x): Căn bậc 2 của x
+    }
+}
+
+/*
+
+Cho một số thực không âm a, hãy tính B = căn_bậc_2(a) và in kết quả ra màn hình với hai chữ số thập phân.
+
+
+ */
