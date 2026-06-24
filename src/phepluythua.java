239347f93fea1b41e9f7341dc69e6f76e5bc6ced
VNGrim <dodinhvan01@gmail.com>
2026-06-24 12:06:09 +0700

    Add

--- src/phepluythua.java
+++ src/phepluythua.java
@@ -1,4 +1,27 @@
+/*
+Luỹ thừa:
+- Math.pow(a,b) - Trả về double, số lớn có thể mất chính xác hoặc tràn
+- BigInteger - Dùng cho số mũ lớn
+ */
+/*Math.pow(a,b)
+VD:
+- double kq = Math.pow(2,4) => kq = 16.(0);
+- int kq = (int) Math.pow(2,4) => kq = 16;
+ */
+/*BigInteger
+import java.math.BigInteger; - Khai báo thư viện
+VD:
+- BigInteger a = a.pow(2); => a = a^2;
+ */
+import java.util.Scanner;
+import java.math.BigInteger;
 public class phepluythua {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        System.out.println(a.pow(2));
+        System.out.println(a.pow(5));
+    }
 }
 
 /*
